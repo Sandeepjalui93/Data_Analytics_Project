6230ebda05f4babe1404bb22776d8cc1f73d7d6e
Sandeep Jalui <50951747+Sandeepjalui93@users.noreply.github.com>
2023-06-20 18:02:30 -0400

    Add files via upload

--- Myntra_product_data_analysis/main.py
+++ Myntra_product_data_analysis/main.py
@@ -1,1 +1,33 @@
+import pandas as pd
+import mysql.connector as connection
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
 
+HOST = 'localhost'
+USERNAME = 'root'
+PASSWORD = ''
+DATABASE = 'myntra'
+
+def connecting_db_and_loading_data():
+    query = "select * from cleandata;"
+    mydb = connection.connect(host=HOST, port="3306", database=DATABASE, user=USERNAME, passwd=PASSWORD)
+    df = pd.read_sql(query, mydb)
+    return df
+
+def top_ratings():
+    data = connecting_db_and_loading_data()
+    return len(data[data["avg_rating"]>4]["ratingCount"]>2000)
+
+print("There are",top_ratings(),"ratings for avg_rating above 4 and ratingcount above 2000")
+
+data = connecting_db_and_loading_data()
+new_data = data[(data['avg_rating']>4.4)&(data['ratingCount']>3000)]
+print('The top 10 products with avg_rating above 4.4 and ratingcount above 3000 are',new_data.head(10))
+
+#Average rating plot
+gk=connecting_db_and_loading_data().groupby(["brand"]).agg({"avg_rating":["mean"]})
+xaxis=gk.index.values
+yaxis=gk.reset_index()['avg_rating']['mean']
+plt.plot(xaxis,yaxis)
+plt.show()
